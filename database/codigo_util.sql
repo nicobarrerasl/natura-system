fed4b142e86d82a1f36b498b2bd5a8f283908f76
FrancoPergolini <francop06.fp@gmail.com>
2020-11-16 01:18:42 -0300

    Agregue un insert de varios productos a postgre dentro del txt

--- database/codigo_util.sql
+++ database/codigo_util.sql
@@ -1,8 +1,15 @@
 INSERT INTO 
     Cliente(cliente_apellido, cliente_nombre, cliente_edad, cliente_Fnac, cliente_tel,cliente_DV,cliente_ZV) 
-VALUES ('Barrera', 'Nicolas','25', '1995-05-23', '266-5033303', 'Sarmiento 790', 'Centro'),
-       ('Pergolini', 'Franco','26', '1994-04-12', '265-7033303', 'Sarmiento 23', 'Centro'),
-       ('Perez', 'Ester','50', '1970-05-23', '266-4033303', 'Rivad. 123', 'Centro'),
-       ('Barroso', 'Jorge','22', '1998-05-23', '266-4333303', 'Colon 20', 'Centro')
+VALUES ('Barrera', 'Nicolas', '1995-05-23', '266-5033303', 'Sarmiento 790', 'Centro'),
+       ('Pergolini', 'Franco', '1994-04-12', '265-7033303', 'Sarmiento 23', 'Centro'),
+       ('Perez', 'Ester', '1970-05-23', '266-4033303', 'Rivad. 123', 'Centro'),
+       ('Barroso', 'Jorge', '1998-05-23', '266-4333303', 'Colon 20', 'Centro')
+
+INSERT INTO 
+    Producto(codProducto, prod_cat, prod_linea, prod_nombre, prod_precio, prod_cant) 
+VALUES (755895, 'MAMA y BEBE', 'TODO DIA', 'KAIAK', 999.1, 5),
+       (945895, 'HOMBRE', 'VER PARA CREER', 'BODY SPLASH', 559.9, 10),
+       (545854, 'MUJER', 'CREMAS', 'MANUSA', 1500, 7),
+       (122357, 'MUJER', 'EKOS', 'ESSENSIAL', 230.5, 2)
 
 select * from cliente
